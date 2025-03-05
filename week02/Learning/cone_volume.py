fd953d70fca601b97c7225fc578ce841ebace9c4
MBarker2BYU <mbarker2@byupathway.edu>
2025-03-05 12:15:58 -0500

    Learning Variable Scope

--- week02/Learning/cone_volume.py
+++ week02/Learning/cone_volume.py
@@ -1,10 +1,11 @@
 # Author: Matthew D. Barker
 # Date: Mar 05, 2025
-# Class: Learning
+# Class: ConeVolume
 
-# Description: Learning Functions week 2
+# Description: Learning exercise 04 Checkpoint : Variable Scope
 
-from os import system, name, path
+from os import system, name
+from math import pi as PI
 
 class Utilities:
 
@@ -103,48 +104,32 @@ class Utilities:
         if clear_screen_after:
             Utilities.clear_screen()
 
-class Learning:
+class ConeVolume:
   
+  #Calculate the volume of a right cone
   @staticmethod
-  def miles_per_gallon(odometer_start, odometer_end, gallons):
+  def cone_volume(radius, height):
       
-      return (odometer_end - odometer_start) / gallons
-  
-  @staticmethod  
-  def lp_100K_mpg(mpg):
-      
-      return 235.215 / mpg
-
+      return PI * radius**2 * height / 3
 # Included to meet assignment requirements only
 def main():
-    
-    Utilities.clear_screen()
+            
+    Utilities.print_plus("This application will calculate the volume of a right cone.", True, 2, 2)
 
-    Utilities.spacing(2)
+    radius = Utilities.input_number("What is the radius of the base of the cone? ", "Radius", True)
+    height = Utilities.input_number("What is the height of the cone? ", "Height", True)
 
-    odometer_start = Utilities.input_number("What was the starting odometer value? ", "odometer reading", True)
-    odometer_end = Utilities.input_number("What was the ending odometer value? ", "odometer reading", True)
-    gallons_used = Utilities.input_number("How many gallons were used? ", "gallons", True)
+    volume = cone_volume(radius, height)
 
-    mpg = miles_per_gallon(odometer_start, odometer_end, gallons_used)
-    liters_per_100_km = lp100k_from_mpg(mpg)
-
-    Utilities.spacing(2)
-
-    Utilities.input_plus(F"{odometer_end - odometer_start:,.1f} miles were traveled using {gallons_used} gallons of fuel for an average of {mpg:,.1f} miles per gallon ({liters_per_100_km:,.2f} liters per one hundred kilometers).", True)
-
-    Utilities.spacing(2)
+    Utilities.print_plus(F"A right cone with a radius of {radius} at the base and a height of {height}, has a volume of {volume:,.1f}.", spacing_before=1)
 
 # Included to meet assignment requirements only
-def miles_per_gallon(start_miles, end_miles, amount_gallons):
-    return Learning.miles_per_gallon(start_miles, end_miles, amount_gallons)
+def cone_volume(radius, height):
 
-# Included to meet assignment requirements only
-def lp100k_from_mpg(mpg):
-    return Learning.lp_100K_mpg(mpg)
-    
+    return ConeVolume.cone_volume(radius, height)
 
 if __name__=="__main__":
 
     # Included to meet assignment requirements only
     main()
+    
