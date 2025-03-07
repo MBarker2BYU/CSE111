df7dfed004cd3ca71bb8dbde2676d5c2d1279629
MBarker2BYU <mbarker2@byupathway.edu>
2025-03-07 15:05:32 -0500

    Water Pressure Complete

--- week03/WaterPressure/test_water_flow.py
+++ week03/WaterPressure/test_water_flow.py
@@ -1,14 +1,40 @@
 # Author: Matthew D. Barker
-# Date: Mar 06, 2025
+# Date: Mar 07, 2025
 # Description: Milestone: Water Pressure
 
 from pytest import approx
 import pytest
+import inspect
 
 from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe
 from water_flow import pressure_loss_from_fittings, reynolds_number, pressure_loss_from_pipe_reduction
+from water_flow import kpa_to_psi
+
+# Begin Real World External
+
+class Tests:
+    # Flag as not to be tested    
+    __test__ = False
+
+    def __init__(self):
+        
+        self._tests = {}
+
+    def append(self, test_data):
+        
+        if not self._tests.__contains__(test_data.method_name):
+            self._tests[test_data.method_name] = []
+        
+        self._tests[test_data.method_name].append(test_data)
+
+    def get_tests(self, name):
+        
+        return self._tests[name]
 
 class TestData:
+    # Flag as not to be tested    
+    __test__ = False
+
     def __init__ (self, method_name, *parameters):
         
         self._method_name = method_name
@@ -27,20 +53,20 @@ class TestData:
 
         return value
     
-tests = []
+tests = Tests()
 
-#water_column_height 
+# water_column_height 
 tests.append(TestData('water_column_height', 0.0, 0.0, 0.0))
 tests.append(TestData('water_column_height', 0.0, 10.0, 7.5))
 tests.append(TestData('water_column_height', 25.0, 0.0, 25.0))
 tests.append(TestData('water_column_height', 48.3, 12.8, 57.9))
 
-#pressure_gain_from_water_height
+# pressure_gain_from_water_height
 tests.append(TestData('pressure_gain_from_water_height', 0.0, 0.000, 0.001))
 tests.append(TestData('pressure_gain_from_water_height', 30.2, 295.628, 0.001))
 tests.append(TestData('pressure_gain_from_water_height', 50.0, 489.450, 0.001))
 
-#pressure_loss_from_pipe
+# pressure_loss_from_pipe
 tests.append(TestData('pressure_loss_from_pipe', 0.048692, 0.00, 0.018, 1.75, 0.000, 0.001))
 tests.append(TestData('pressure_loss_from_pipe', 0.048692,	200.00,	0.000, 1.75, 0.000,	0.001))
 tests.append(TestData('pressure_loss_from_pipe', 0.048692,	200.00,	0.018, 0.00, 0.000,	0.001))
@@ -49,74 +75,127 @@ tests.append(TestData('pressure_loss_from_pipe', 0.048692,	200.00,	0.018, 1.65, 
 tests.append(TestData('pressure_loss_from_pipe', 0.286870,	1000.00, 0.013,	1.65, -61.576, 0.001))
 tests.append(TestData('pressure_loss_from_pipe', 0.286870,	1800.75, 0.013,	1.65, -110.884,	0.001))
 
+# test_pressure_loss_from_fittings
+tests.append(TestData('pressure_loss_from_fittings', 0.00,	3, 0.000, 0.001))
+tests.append(TestData('pressure_loss_from_fittings', 1.65,	0, 0.000, 0.001))
+tests.append(TestData('pressure_loss_from_fittings', 1.65,	2, -0.109, 0.001))
+tests.append(TestData('pressure_loss_from_fittings', 1.75,	2, -0.122, 0.001))
+tests.append(TestData('pressure_loss_from_fittings', 1.75,	5, -0.306, 0.001))
+
+# test_reynolds_number
+tests.append(TestData('reynolds_number', 0.048692,	0.00, 0, 1))
+tests.append(TestData('reynolds_number', 0.048692,	1.65, 80069, 1))
+tests.append(TestData('reynolds_number', 0.048692,	1.75, 84922, 1))
+tests.append(TestData('reynolds_number', 0.286870,	1.65, 471729, 1))
+tests.append(TestData('reynolds_number', 0.286870, 1.75, 500318, 1))
+
+# test_pressure_loss_from_pipe_reduction
+tests.append(TestData('pressure_loss_from_pipe_reduction', 0.28687, 0.00, 1, 0.048692, 0.000, 0.001))
+tests.append(TestData('pressure_loss_from_pipe_reduction', 0.28687,	1.65, 471729, 0.048692, -163.744, 0.001))
+tests.append(TestData('pressure_loss_from_pipe_reduction', 0.28687,	1.75, 500318, 0.048692, -184.182, 0.001))
+
+# test_kpa_to_psi
+tests.append(TestData('kpa_to_psi', 1, 0.145038, 0.01))
+tests.append(TestData('kpa_to_psi', 5, 0.725189, 0.01))
+tests.append(TestData('kpa_to_psi', 13, 1.88549, 0.01))
+tests.append(TestData('kpa_to_psi', 27, 3.91602, 0.01))
+tests.append(TestData('kpa_to_psi', 71, 10.2977, 0.01))
+tests.append(TestData('kpa_to_psi', 1701, 246.7092, 0.01))
+tests.append(TestData('kpa_to_psi', 1776, 257.587, 0.01))
+
+
+# End Real world External 
+
+
 def validate_float_type(name, result):
 
     assert isinstance(result, float), F"{name} function must return a float"
 
+def method_name():
+    return inspect.stack()[1][3].replace('test_', '')
+
 
 def test_water_column_height():
-    
-    # result = water_column_height(0.0, 0.0)
-    # assert isinstance(result, float), F"{water_column_height} function must return a float"
 
-    validate_float_type('water_column_height', water_column_height(0.0, 0.0))
+    # Get the method name from the stack and remover 'test_' to get the method being tested    
+    name = method_name()
 
-    for test_data in tests:
-        if not test_data.method_name == 'water_column_height':
-            continue
-        
+    validate_float_type(name, water_column_height(0.0, 0.0))
+
+    # Get the tests for the the give menthod
+    for test_data in tests.get_tests(name):
+                
         assert water_column_height(test_data.test_parameter(0), test_data.test_parameter(1)) == test_data.test_parameter(2)
 
 def test_pressure_gain_from_water_height():
-    
-    # result = pressure_gain_from_water_height(0.0)
-    # assert isinstance(result, float), F"{test_pressure_gain_from_water_height} function must return a float"
 
-    validate_float_type('pressure_gain_from_water_height', pressure_gain_from_water_height(0.0))
+    # Get the method name from the stack and remover 'test_' to get the method being tested    
+    name = method_name() 
 
-    for test_data in tests:
-        if not test_data.method_name == 'pressure_gain_from_water_height':
-            continue
-        
+    validate_float_type(name, pressure_gain_from_water_height(0.0))
+
+    for test_data in tests.get_tests(name):
+                
         assert pressure_gain_from_water_height(test_data.test_parameter(0)) == approx(test_data.test_parameter(1), abs=test_data.test_parameter(2))
 
 
 def test_pressure_loss_from_pipe():
-    
-    # result = pressure_loss_from_pipe(0.0, 0.0, 0.0, 0.0)
-    # assert isinstance(result, float), F"{test_pressure_gain_from_water_height} function must return a float"
 
-    validate_float_type('pressure_loss_from_pipe', pressure_loss_from_pipe(0.0, 0.0, 0.0, 0.0))
+    # Get the method name from the stack and remover 'test_' to get the method being tested    
+    name = method_name()
 
-    for test_data in tests:
-        if not test_data.method_name == 'pressure_loss_from_pipe':
-            continue
+    validate_float_type(name, pressure_loss_from_pipe(0.0, 0.0, 0.0, 0.0))
+
+    for test_data in tests.get_tests(name):
+        # if not test_data.method_name == 'pressure_loss_from_pipe':
+        #     continue
         
-        assert pressure_loss_from_pipe(test_data.test_parameter(0),test_data.test_parameter(1),test_data.test_parameter(2),test_data.test_parameter(3)) == approx(test_data.test_parameter(4), test_data.test_parameter(5))
+        assert pressure_loss_from_pipe(test_data.test_parameter(0),test_data.test_parameter(1),test_data.test_parameter(2),test_data.test_parameter(3)) == approx(test_data.test_parameter(4), abs=test_data.test_parameter(5))
 
 def test_pressure_loss_from_fittings():
+    
+    # Get the method name from the stack and remover 'test_' to get the method being tested    
+    name = method_name()
 
-    # result = pressure_loss_from_pipe(0.0, 0.0, 0.0, 0.0)
-    # assert isinstance(result, float), F"{test_pressure_loss_from_fittings} function must return a float"
+    validate_float_type(name, pressure_loss_from_fittings(0.0, 0.0))
 
-    validate_float_type('pressure_loss_from_fittings', pressure_loss_from_fittings(0.0, 0.0))
-
-    return 0.0
+    for test_data in tests.get_tests(name):
+        
+        assert pressure_loss_from_fittings(test_data.test_parameter(0),test_data.test_parameter(1)) == approx(test_data.test_parameter(2), abs=test_data.test_parameter(3))
 
 def test_reynolds_number():
-    
-    # result = pressure_loss_from_pipe(0.0, 0.0, 0.0, 0.0)
-    # assert isinstance(result, float), F"{test_pressure_loss_from_fittings} function must return a float"
 
-    validate_float_type('reynolds_number', reynolds_number(0.0, 0.0))
+    # Get the method name from the stack and remover 'test_' to get the method being tested    
+    name = method_name()
 
-    return 0.0
+    validate_float_type(name, reynolds_number(0.0, 0.0))
+
+    for test_data in tests.get_tests(name):
+        
+        assert reynolds_number(test_data.test_parameter(0),test_data.test_parameter(1)) == approx(test_data.test_parameter(2), abs=test_data.test_parameter(3))
 
 def test_pressure_loss_from_pipe_reduction():
 
-    validate_float_type('pressure_loss_from_pipe_reduction', pressure_loss_from_pipe_reduction(0.0, 0.0, 0.0, 0.0))
+    # Get the method name from the stack and remover 'test_' to get the method being tested    
+    name = method_name()
 
-    return 0.0
+    validate_float_type(name, pressure_loss_from_pipe_reduction(0.0, 0.0, 0.0, 0.0))
+
+    for test_data in tests.get_tests(name):
+
+        assert pressure_loss_from_pipe_reduction(test_data.test_parameter(0), test_data.test_parameter(1), test_data.test_parameter(2), test_data.test_parameter(3)) == approx(test_data.test_parameter(4), abs=test_data.test_parameter(5))
+
+def test_kpa_to_psi():
+
+    # Get the method name from the stack and remover 'test_' to get the method being tested    
+    name = method_name()
+
+    validate_float_type(name, kpa_to_psi(0.0))
+
+    for test_data in tests.get_tests(name):
+        
+        assert kpa_to_psi(test_data.test_parameter(0)) == approx(test_data.test_parameter(1), abs=test_data.test_parameter(2))
+
 
 
 # Call the main function that is part of pytest so that the
--- week03/WaterPressure/water_flow.py
+++ week03/WaterPressure/water_flow.py
@@ -2,10 +2,13 @@
 # Date: Mar 06, 2025
 # Description: Milestone: Water Pressure
 
-DENSITY = 998.2
-GRAVITY = 9.80665
-DYNAMIC_VISCOSITY_OF_WATER = 0.0010016
+from os import system, name
 
+WATER_DENSITY = 998.2000000
+EARTH_ACCELERATION_OF_GRAVITY = 9.8066500
+WATER_DYNAMIC_VISCOSITY = 0.0010016
+
+#Safe Division (Check for division by zero)
 def safe_division_float(dividend, divisor):
 
     if(divisor == 0.0):
@@ -13,38 +16,51 @@ def safe_division_float(dividend, divisor):
     
     return float(dividend / divisor)
 
+
 def water_column_height(tower_height, tank_height):
    
     return tower_height + (3 * safe_division_float(tank_height, 4))
- 
+
+
 def pressure_gain_from_water_height(height):
 
-    return DENSITY * GRAVITY * safe_division_float(height, 1000)
+    return WATER_DENSITY * EARTH_ACCELERATION_OF_GRAVITY * safe_division_float(height, 1000)
+
 
 def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_velocity):
     
-    pressure = safe_division_float(-(friction_factor * pipe_length * DENSITY * fluid_velocity**2), (2000 * pipe_diameter))
+    pressure = safe_division_float(-(friction_factor * pipe_length * WATER_DENSITY * fluid_velocity**2), (2000 * pipe_diameter))
     
     #do not return a -0.0
     return pressure if abs(pressure) else 0.0    
 
+
 def pressure_loss_from_fittings(fluid_velocity, quantity_fittings):
     
-    return -0.004 * DENSITY * fluid_velocity**2 * safe_division_float(quantity_fittings, 2000)
+    pressure = -0.04 * WATER_DENSITY * fluid_velocity**2 * safe_division_float(quantity_fittings, 2000)
+
+    #do not return a -0.0
+    return pressure if abs(pressure) else 0.0
 
 
 def reynolds_number(hydraulic_diameter, fluid_velocity):
     
-    return safe_division_float(DENSITY * hydraulic_diameter * fluid_velocity,  DYNAMIC_VISCOSITY_OF_WATER)
+    return safe_division_float(WATER_DENSITY * hydraulic_diameter * fluid_velocity,  WATER_DYNAMIC_VISCOSITY)
+
 
 def pressure_loss_from_pipe_reduction(larger_diameter, fluid_velocity, reynolds_number, smaller_diameter):
     
-    k = safe_division_float(0.1 + 50 , reynolds_number) * ( safe_division_float(larger_diameter, smaller_diameter)**4 - 1)
-
-    pressure = 0.0
+    k = (0.1 + safe_division_float(50 , reynolds_number)) * ( safe_division_float(larger_diameter, smaller_diameter)**4 - 1)
+    
+    pressure = safe_division_float(-(k * WATER_DENSITY * fluid_velocity**2), 2000)
 
     #do not return a -0.0
     return pressure if abs(pressure) else 0.0 
+
+# Convert kPa to psi
+def kpa_to_psi(kpa):
+
+    return kpa / 6.895
 
 #***********************************************************************
 # -- Provided Code --
@@ -57,7 +73,21 @@ HDPE_SDR11_INNER_DIAMETER = 0.048692 # (meters)  1.917 inches
 HDPE_SDR11_FRICTION_FACTOR = 0.018   # (unitless)
 HOUSEHOLD_VELOCITY = 1.75            # (meters / second)
 
+def clear_screen():
+
+    system("cls" if name == "nt" else "clear")
+
+def spacing(lines = 1):
+
+        for _ in range(lines):
+            print("")
+
 def main():
+
+    # Cleaner Look
+    clear_screen()
+    spacing(2)
+
     tower_height = float(input("Height of water tower (meters): "))
     tank_height = float(input("Height of water tank walls (meters): "))
     length1 = float(input("Length of supply pipe from tank to lot (meters): "))
@@ -81,7 +111,14 @@ def main():
     velocity = HOUSEHOLD_VELOCITY
     loss = pressure_loss_from_pipe(diameter, length2, friction, velocity)
     pressure += loss
-    print(f"Pressure at house: {pressure:.1f} kilopascals")
+
+    spacing()
+
+    print(f"Pressure at house: {pressure:.1f} kilopascals ({kpa_to_psi(pressure):,.1f} psi)")
+
+    # Cleaner Look
+    spacing(2)
+
 
 if __name__ == "__main__":
     main()
