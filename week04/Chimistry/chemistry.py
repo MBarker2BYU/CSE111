4f40f3a349ba5eb40574416f7445e1ddac412966
MBarker2BYU <mbarker2@byupathway.edu>
2025-03-28 08:57:18 -0400

    Updates

--- week04/Chimistry/chemistry.py
+++ week04/Chimistry/chemistry.py
@@ -2,9 +2,111 @@ from formula import parse_formula
 from os import system, name
 from enum import Enum
 import os
-import csv
+# import csv
+
+def element_info():
+        
+    elements = []
+
+    elements.append(['Ac','Actinium',227.0, 89, 89, 138, 89])
+    elements.append(['Ag','Silver',107.8682, 47, 47, 61, 47])
+    elements.append(['Al','Aluminum',26.9815386, 13, 13, 14, 13])
+    elements.append(['Ar','Argon',39.948, 18, 18, 22, 18])
+    elements.append(['As','Arsenic',74.9216, 33, 33, 42, 33])
+    elements.append(['At','Astatine',210.0, 85, 85, 125, 85])
+    elements.append(['Au','Gold',196.966569, 79, 79, 118, 79])
+    elements.append(['B','Boron',10.811, 5, 5, 6, 5])
+    elements.append(['Ba','Barium',137.327, 56, 56, 81, 56])
+    elements.append(['Be','Beryllium',9.012182, 4, 4, 5, 4])
+    elements.append(['Bi','Bismuth',208.9804, 83, 83, 126, 83])
+    elements.append(['Br','Bromine',79.904, 35, 35, 45, 35])
+    elements.append(['C','Carbon',12.0107, 6, 6, 6, 6])
+    elements.append(['Ca','Calcium',40.078, 20, 20, 20, 20])
+    elements.append(['Cd','Cadmium',112.411, 48, 48, 64, 48])
+    elements.append(['Ce','Cerium',140.116, 58, 58, 82, 58])
+    elements.append(['Cl','Chlorine',35.453, 17, 17, 18, 17])
+    elements.append(['Co','Cobalt',58.933195, 27, 27, 32, 27])
+    elements.append(['Cr','Chromium',51.9961, 24, 24, 28, 24])
+    elements.append(['Cs','Cesium',132.9054519, 55, 55, 78, 55])
+    elements.append(['Cu','Copper',63.546, 29, 29, 35, 29])
+    elements.append(['Dy','Dysprosium',162.5, 66, 66, 97, 66])
+    elements.append(['Er','Erbium',167.259, 68, 68, 99, 68])
+    elements.append(['Eu','Europium',151.964, 63, 63, 89, 63])
+    elements.append(['F','Fluorine',18.9984032, 9, 9, 10, 9])
+    elements.append(['Fe','Iron',55.845, 26, 26, 30, 26])
+    elements.append(['Fr','Francium',223.0, 87, 87, 136, 87])
+    elements.append(['Ga','Gallium',69.723, 31, 31, 39, 31])
+    elements.append(['Gd','Gadolinium',157.25, 64, 64, 93, 64])
+    elements.append(['Ge','Germanium',72.64, 32, 32, 41, 32])
+    elements.append(['H','Hydrogen',1.00794, 1, 1, 0, 1])
+    elements.append(['He','Helium',4.002602, 2, 2, 2, 2])
+    elements.append(['Hf','Hafnium',178.49, 72, 72, 106, 72])
+    elements.append(['Hg','Mercury',200.59, 80, 80, 121, 80])
+    elements.append(['Ho','Holmium',164.93032, 67, 67, 98, 67])
+    elements.append(['I','Iodine',126.90447, 53, 53, 74, 53])
+    elements.append(['In','Indium',114.818, 49, 49, 66, 49])
+    elements.append(['Ir','Iridium',192.217, 77, 77, 115, 77])
+    elements.append(['K','Potassium',39.0983, 19, 19, 20, 19])
+    elements.append(['Kr','Krypton',83.798, 36, 36, 48, 36])
+    elements.append(['La','Lanthanum',138.90547, 57, 57, 82, 57])
+    elements.append(['Li','Lithium',6.941, 3, 3, 4, 3])
+    elements.append(['Lu','Lutetium',174.9668, 71, 71, 104, 71])
+    elements.append(['Mg','Magnesium',24.305, 12, 12, 12, 12])
+    elements.append(['Mn','Manganese',54.938045, 25, 25, 30, 25])
+    elements.append(['Mo','Molybdenum',95.96, 42, 42, 54, 42])
+    elements.append(['N','Nitrogen',14.0067, 7, 7, 7, 7])
+    elements.append(['Na','Sodium',22.98976928, 11, 11, 12, 11])
+    elements.append(['Nb','Niobium',92.90638, 41, 41, 52, 41])
+    elements.append(['Nd','Neodymium',144.242, 60, 60, 84, 60])
+    elements.append(['Ne','Neon',20.1797, 10, 10, 10, 10])
+    elements.append(['Ni','Nickel',58.6934, 28, 28, 31, 28])
+    elements.append(['Np','Neptunium',237.0, 93, 93, 144, 93])
+    elements.append(['O','Oxygen',15.9994, 8, 8, 8, 8])
+    elements.append(['Os','Osmium',190.23, 76, 76, 114, 76])
+    elements.append(['P','Phosphorus',30.973762, 15, 15, 16, 15])
+    elements.append(['Pa','Protactinium',231.03588, 91, 91, 140, 91])
+    elements.append(['Pb','Lead',207.2, 82, 82, 125, 82])
+    elements.append(['Pd','Palladium',106.42, 46, 46, 60, 46])
+    elements.append(['Pm','Promethium',145.0, 61, 61, 84, 61])
+    elements.append(['Po','Polonium',209.0, 84, 84, 126, 84])
+    elements.append(['Pr','Praseodymium',140.90765, 59, 59, 82, 59])
+    elements.append(['Pt','Platinum',195.084, 78, 78, 117, 78])
+    elements.append(['Pu','Plutonium',244.0, 94, 94, 150, 94])
+    elements.append(['Ra','Radium',226.0, 88, 88, 138, 88])
+    elements.append(['Rb','Rubidium',85.4678, 37, 37, 48, 37])
+    elements.append(['Re','Rhenium',186.207, 75, 75, 111, 75])
+    elements.append(['Rh','Rhodium',102.9055, 45, 45, 58, 45])
+    elements.append(['Rn','Radon',222.0, 86, 86, 136, 86])
+    elements.append(['Ru','Ruthenium',101.07, 44, 44, 57, 44])
+    elements.append(['S','Sulfur',32.065, 16, 16, 16, 16])
+    elements.append(['Sb','Antimony',121.76, 51, 51, 71, 51])
+    elements.append(['Sc','Scandium',44.955912, 21, 21, 24, 21])
+    elements.append(['Se','Selenium',78.96, 34, 34, 45, 34])
+    elements.append(['Si','Silicon',28.0855, 14, 14, 14, 14])
+    elements.append(['Sm','Samarium',150.36, 62, 62, 88, 62])
+    elements.append(['Sn','Tin',118.71, 50, 50, 69, 50])
+    elements.append(['Sr','Strontium',87.62, 38, 38, 50, 38])
+    elements.append(['Ta','Tantalum',180.94788, 73, 73, 108, 73])
+    elements.append(['Tb','Terbium',158.92535, 65, 65, 94, 65])
+    elements.append(['Tc','Technetium',98.0, 43, 43, 55, 43])
+    elements.append(['Te','Tellurium',127.6, 52, 52, 76, 52])
+    elements.append(['Th','Thorium',232.03806, 90, 90, 142, 90])
+    elements.append(['Ti','Titanium',47.867, 22, 22, 26, 22])
+    elements.append(['Tl','Thallium',204.3833, 81, 81, 123, 81])
+    elements.append(['Tm','Thulium',168.93421, 69, 69, 100, 69])
+    elements.append(['U','Uranium',238.02891, 92, 92, 146, 92])
+    elements.append(['V','Vanadium',50.9415, 23, 23, 28, 23])
+    elements.append(['W','Tungsten',183.84, 74, 74, 110, 74])
+    elements.append(['Xe','Xenon',131.293, 54, 54, 77, 54])
+    elements.append(['Y','Yttrium',88.90585, 39, 39, 50, 39])
+    elements.append(['Yb','Ytterbium',173.054, 70, 70, 103, 70])
+    elements.append(['Zn','Zinc',65.38, 30, 30, 35, 30])
+    elements.append(['Zr','Zirconium',91.224, 40, 40, 51, 40])
+                    
+    return elements
 
 class Utilities:
+    
     def clear_screen():
         system("cls" if name == "nt" else "clear")
 
@@ -15,14 +117,22 @@ class Utilities:
     def get_current_directory():
         return os.path.dirname(os.path.realpath(__file__))
 
+    def translate_to_subscript(text):
+        SUB = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
+        return text.translate(SUB)    
+    
+    def translate_to_superscript(text):
+        SUP = str.maketrans("0123456789", "⁰¹²³⁴⁵⁶⁷⁸⁹")
+        return text.translate(SUP)
+
 class PeriodicTableItem(Enum):
-    AtomicNumber=0
+    Symbol=0
     Name=1
-    Symbol=2
-    AtomicMass=3
-    NumberofNeutrons=4
-    NumberofProtons=5
-    NumberofElectrons=6
+    AtomicMass=2
+    AtomicNumber=3
+    NumberofElectrons=4
+    NumberofNeutrons=5
+    NumberofProtons=6
 
 class Element:
     def __init__(self, symbol, name, atomic_mass, atomic_number=0, number_of_electrons=0, number_of_neutrons=0, number_of_protons=0):
@@ -77,52 +187,64 @@ class Element:
     def to_list_item_extended(self):
         return [self._name, self._atomic_mass, self._symbol, self._atomic_number, self._number_of_electrons, self._number_of_neutrons, self._number_of_protons]
 
-
 class PeriodicTable:
-    def __init__(self, element_file="ElementInfo.csv", extended_element_file="ExtendedElementInfo.csv"):
-        self._Initialize(element_file, extended_element_file)
+    def __init__(self):
+        self._Initialize()
 
-    def _Initialize(self, element_file, extended_element_file):
+    def _Initialize(self):
         
         self._elements = {}
 
-        element_info_path = F"{Utilities.get_current_directory()}\\{element_file}"
+        els = element_info()
 
-        with open(element_info_path, 'r') as file:
-            csv_reader = csv.reader(file)
-            header = next(csv_reader)
-            element_data = list(csv_reader)
+        for e in els:
+            self.add_element(Element(e[PeriodicTableItem.Symbol.value],
+                                     e[PeriodicTableItem.Name.value],
+                                     e[PeriodicTableItem.AtomicMass.value],
+                                     e[PeriodicTableItem.AtomicNumber.value],
+                                     e[PeriodicTableItem.NumberofElectrons.value],
+                                     e[PeriodicTableItem.NumberofNeutrons.value],
+                                     e[PeriodicTableItem.NumberofProtons.value]
+                                     ))
 
-            for element in element_data:
+        
+        # element_info_path = F"{Utilities.get_current_directory()}\\{element_file}"
 
-                symbol = element[0].strip()
-                name = element[1].strip()
-                atomic_mass = float(element[2].strip())
+        # with open(element_info_path, 'r') as file:
+        #     csv_reader = csv.reader(file)
+        #     header = next(csv_reader)
+        #     element_data = list(csv_reader)
 
-                self.add_element(Element(symbol, name, atomic_mass))
+        #     for element in element_data:
 
+        #         symbol = element[0].strip()
+        #         name = element[1].strip()
+        #         atomic_mass = float(element[2].strip())
 
-        extended_element_info = F"{Utilities.get_current_directory()}\\{extended_element_file}"
+        #         self.add_element(Element(symbol, name, atomic_mass))
+            
 
-        with open(extended_element_info, 'r') as file:
-            csv_reader = csv.reader(file)
-            header = next(csv_reader)
-            extended_element_data = list(csv_reader)
+        # extended_element_info = F"{Utilities.get_current_directory()}\\{extended_element_file}"
 
-            for element in extended_element_data:
+        # with open(extended_element_info, 'r') as file:
+        #     csv_reader = csv.reader(file)
+        #     header = next(csv_reader)
+        #     extended_element_data = list(csv_reader)            
+
+        #     for element in extended_element_data:
                 
-                symbol = element[PeriodicTableItem.Symbol.value].strip()
+        #         symbol = element[PeriodicTableItem.Symbol.value].strip()
 
-                if not self.has_key(symbol):
-                    continue
+        #         if not self.has_key(symbol):
+        #             continue
 
-                atomic_number = int(element[PeriodicTableItem.AtomicNumber.value].strip())
-                electrons = int(element[PeriodicTableItem.NumberofElectrons.value].strip())
-                neutrons = int(element[PeriodicTableItem.NumberofNeutrons.value].strip())
-                protons = int(element[PeriodicTableItem.NumberofProtons.value].strip())
+        #         atomic_number = int(element[PeriodicTableItem.AtomicNumber.value].strip())
+        #         electrons = int(element[PeriodicTableItem.NumberofElectrons.value].strip())
+        #         neutrons = int(element[PeriodicTableItem.NumberofNeutrons.value].strip())
+        #         protons = int(element[PeriodicTableItem.NumberofProtons.value].strip())
 
-                self.get_element(symbol).update(atomic_number, electrons, neutrons, protons)
-
+        #         self.get_element(symbol).update(atomic_number, electrons, neutrons, protons)
+        
 
     def add_element(self, element):
         
@@ -162,8 +284,7 @@ def make_periodic_table():
 
     if periodic_table is None:
         periodic_table = PeriodicTable()
-
-    # return periodic_table.to_list()
+    
     return periodic_table.to_dictionary()
     
     
@@ -216,10 +337,10 @@ def get_formula_name(formula, known_molecules_dict):
     else:
         return "unknown compound"
 
-ATOMIC_NUMBER = 3
-ELECTRONS = 4
-NEUTRONS = 5
-PROTONS = 6
+# ATOMIC_NUMBER = 3
+# ELECTRONS = 4
+# NEUTRONS = 5
+# PROTONS = 6
 
 def sum_electrons(symbol_quantity_list, periodic_table_dict):
 
@@ -292,18 +413,21 @@ def main():
 
         Utilities.clear_screen()
 
-        print(F"Formula Name: {formula_name.title()}")
+        print("Molecule Data:")
+        print(F"{F"  Formula: ".ljust(25,'.')}{F"{Utilities.translate_to_subscript(response)}".rjust(25,'.')}")
+        print(F"{F"  Name: ".ljust(25,'.')}{F"{formula_name.title()}".rjust(25,'.')}")
+        print(F"{F"  Electrons: ".ljust(25,'.')}{F"{electron_count}".rjust(25,'.')}")
+        print(F"{F"  Neutrons: ".ljust(25,'.')}{F"{neutron_count}".rjust(25,'.')}")
+        print(F"{F"  Protons: ".ljust(25,'.')}{F"{proton_count}".rjust(25,'.')}")
         print()
-        print(F"{F"Formula:".ljust(25,'.')}{F"{response}".rjust(25,'.')}")
-        print(F"{F"Mass:".ljust(25,'.')}{F"{mass} grams".rjust(25,'.')}")
-        print(F"{F"grams/mole:".ljust(25,'.')}{F"{data:,.5f}".rjust(25,'.')}")
-        print(F"{F"moles:".ljust(25,'.')}{F"{moles:,.5f}".rjust(25,'.')}")
-        print(F"{F"Electrons:".ljust(25,'.')}{F"{electron_count}".rjust(25,'.')}")
-        print(F"{F"Neutrons:".ljust(25,'.')}{F"{neutron_count}".rjust(25,'.')}")
-        print(F"{F"Protons:".ljust(25,'.')}{F"{proton_count}".rjust(25,'.')}")
+        print("Sample Data:")
+        print(F"{F"  Mass: ".ljust(25,'.')}{F"{mass} grams".rjust(25,'.')}")
+        print(F"{F"  grams/mole: ".ljust(25,'.')}{F"{data:,.5f}".rjust(25,'.')}")
+        print(F"{F"  moles: ".ljust(25,'.')}{F"{moles:,.5f}".rjust(25,'.')}")
+        
         print()
         
-        if input("Enter another formula?").lower() != 'y':
+        if input("Enter another formula? (y/n)").lower() != 'y':
             break
     
     print()
@@ -311,8 +435,6 @@ def main():
     print("Have a blessed day!")
     print()
     print()
-
-
 
 if __name__ == "__main__":
     main()
